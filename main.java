4d4dfb6704107abfc4acfad3dddf5eaad42ab847
Tsurematsu <96458028+Tsurematsu@users.noreply.github.com>
2022-08-19 01:27:13 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        
+        // https://prod.liveshare.vsengsaas.visualstudio.com/join?28522B5B0C5204F57CE72E7B55F55BA36051
     }
 }
