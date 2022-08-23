0483570d6a716369f6c0040dea3a8951160673a8
Tsurematsu <96458028+Tsurematsu@users.noreply.github.com>
2022-08-23 17:16:56 -0500

    Prueba de comentarios
    Toma tu plata

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         
         System.out.println("Buenas, quiero plata");
+        System.out.println("Toma pizza");
 
     }
 }
