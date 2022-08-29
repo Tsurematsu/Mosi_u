bdba3ba6a1e6fa570d1ab219274b9c3cb0f0bc85
Tsurematsu <96458028+Tsurematsu@users.noreply.github.com>
2022-08-29 10:41:28 -0500

    Cambio
    Hollo moto

---
+++ estudiantes/main_Students.java
@@ -0,0 +1,7 @@
+package estudiantes;
+
+public class main_Students {
+    public static void main(String[] args) {
+        students.panel_estudiantes();
+    }
+}
---
+++ estudiantes/students.java
@@ -0,0 +1,11 @@
+package estudiantes;
+
+public class students {
+    public static void panel_estudiantes() {
+        System.out.println("HOLA");
+        panel2();
+    }
+    public static void panel2(){
+        System.out.println("hoello mto");
+    }
+}
