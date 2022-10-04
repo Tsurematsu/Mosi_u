04cbe544df36acd1183e12a9c3126b691810974a
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2022-10-04 08:42:14 -0500

    Cambio 2

--- administrador/main_enterprise.java
+++ administrador/main_enterprise.java
@@ -5,6 +5,7 @@ import userStorage.*;
 public class main_enterprise {
     public static void main(String[] args) {
         // Methods.ventana = new Methods.ventana();//Genera la ventana para testear el código
-        System.out.println("prueva de funcionamiento");
+        System.out.println("prueba de funcionamiento");
+        System.out.println("Prueba 2");
     }
 }
