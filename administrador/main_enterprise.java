684912a59f591f54c3cc735f6fbd02b6b2b5f520
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2022-10-04 00:52:14 -0500

    Renombrar

--- administrador/main_enterprise.java
+++ administrador/main_enterprise.java
@@ -2,7 +2,7 @@ package administrador;
 import publicStorage.*;
 import userStorage.*;
 
-public class main_admin {
+public class main_enterprise {
     public static void main(String[] args) {
         // Methods.ventana = new Methods.ventana();//Genera la ventana para testear el código
         
--- estudiantes/main_Students.java
+++ estudiantes/main_Students.java
@@ -2,7 +2,7 @@ package estudiantes;
 import publicStorage.*;
 import userStorage.*;
 
-public class main_students {
+public class main_Students {
     public static void main(String[] args) {
         // Methods.ventana = new Methods.ventana();//Genera la ventana para testear el código
 
