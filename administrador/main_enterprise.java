3146269952d9e070a96094340468ca660927e47a
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2022-10-04 00:58:47 -0500

    Subida

--- administrador/main_enterprise.java
+++ administrador/main_enterprise.java
@@ -5,6 +5,6 @@ import userStorage.*;
 public class main_enterprise {
     public static void main(String[] args) {
         // Methods.ventana = new Methods.ventana();//Genera la ventana para testear el código
-        
+        System.out.println("prueva de funcionamiento");
     }
 }
