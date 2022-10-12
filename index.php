ed2940a076e47d346036dd29965665aa3cb5c753
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2022-10-12 15:20:55 -0500

    Actualizacion de seguridad

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+function encode ($valor, $pass) {$method = 'aes-256-cbc';$iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");echo openssl_encrypt ($valor, $method, $pass.base64_encode("75913"), false, $iv);};
+function decode ($valor, $pass) {$method = 'aes-256-cbc';$iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");echo base64_encode(openssl_decrypt($valor, $method, $pass.base64_encode("75913"), false, $iv));};
+function readFilePHP($nombre_fichero, $pass){$fichero_texto = fopen ($nombre_fichero, "r");$contenido_fichero = fread($fichero_texto, filesize($nombre_fichero));echo encode($contenido_fichero, $pass);}
+if (isset($_POST['funct'])) {$func = $_POST['funct']; $func(isset($_POST['param'])? base64_decode($_POST['param']):"", isset($_POST['password'])?$_POST['password']:"_");}else{$password = base64_encode(rand(4357, 9866).'_'.rand(4357, 9866).'_'.rand(4357, 9866));$numRand1 = base64_encode($password);$Ruta = basename(__FILE__);$RutaBase = str_replace("/","", $_SERVER['PHP_SELF']);echo '<script>let Ruta="'.$Ruta.'"; let RutaBase="'.$RutaBase.'";</script>';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moosi_U</title>
+    <link rel="icon" href="favicon.png">
+    <script src="./Imports/jquery-3.6.0.min.js">
+    // <script src="https://code.jquery.com/jquery-3.6.0.min.js">
+    </script><script src="./js/ModuloLoad.js"></script>
+    <?php echo '<script>ModuloLoad.rnmLoad("'.$numRand1.'")</script>'; ?>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <div id='subBody'><div id='contenido'>
+    <script>
+            let prm1 = "<?php encode('./html/Home.html', $password) ?>"
+            let DocSubBody = document.getElementById('contenido')
+            ModuloLoad.load(prm1, DocSubBody).then(()=>{
+                /* En esta parte se ejecuta la logica necesaria cuando finalize la carga de el documento */
+            })
+        </script>
+    </div></div>
+</body>
+</html>
+<?php } ?>
---
+++ js/ModuloLoad.js
@@ -0,0 +1,146 @@
+var ModuloLoad = new function () {
+    let security0 = true;let numerCC;
+    let cachePage=[[],[],[]];
+    let fileScript=Ruta;
+    let SelectPage="";
+    async function readFilePhp(ruta){return decode(await callScript('readFilePHP', ruta, Dec(numerCC)))}
+    async function encode(str) {return callScript("encode", str, Dec(numerCC));}
+    async function decode(str) {return Dec(await callScript("decode", str, Dec(numerCC)));}
+    
+    async function readFile(ruta) {let resultado="";await $.ajax({url : ruta,dataType: "text",success : function (data){resultado = data;}});return resultado;}
+    async function callScript(Funct, Param="", Password="_") {let result="";await $.ajax({method: "POST",url: fileScript,data: {funct:Funct, param:Enc(Param), password:Password},success: function(data) {result = data;},});return result;}
+    async function encodeInText(inWord){let outWord =  inWord;let inText = indicators(inWord, "encode(",")");for (let i = 0; i < inText[0].length; i++) {outWord=outWord.replaceAll(inText[0][i],await encode(inText[1][i]))}return outWord}
+    
+    function Enc( str ) {return window.btoa(unescape(encodeURIComponent( str )));}
+    function Dec( str ) {return decodeURIComponent(escape(window.atob( str )));}
+    function delScript(idScript){var DCT = document.getElementById(idScript); DCT.parentNode.removeChild(DCT);};
+    function indicators(word, open, close) {let inWord= word.split(open);let ListKeys=[[],[]];let count=0;inWord.forEach(element1 => {if (count >0) {let contenido= element1.split(close)[0];ListKeys[1].push(contenido);ListKeys[0].push(open + contenido + close);}else{count++}});return ListKeys}
+    function existList(lista, elemento){let existe=0;for (let i = 0; i < lista.length; i++) {if (existe==0) {if(lista[i]==elemento){existe=i+1/*lista[i];*/}}};return existe;}
+    
+    function ghostScript(textCode, nameId="gost", docLocate=document.getElementsByTagName('body')[0]){
+        let script = document.createElement('script', sedondID=null);
+        let scriptid=Enc(Math.floor(Math.random() * 2080)).replaceAll('.', '').replaceAll('/', '').replaceAll('-', '').replaceAll('=', '');
+        scriptid += "_" +  nameId.replaceAll('.', '').replaceAll('/', '').replaceAll('-', '').replaceAll('=', '');
+        script.id = scriptid;
+        script.innerHTML = textCode;
+        docLocate.appendChild(script);
+        delScript(script.id);
+        if (sedondID!=null) {delScript(sedondID);}
+    };
+
+    function ghostScriptEval(textCode, nameId="gost") {
+        let scriptid=Enc(Math.floor(Math.random() * 2080)).replaceAll('.', '').replaceAll('/', '').replaceAll('-', '').replaceAll('=', '');
+        scriptid += "_" +  nameId.replaceAll('.', '').replaceAll('/', '').replaceAll('-', '').replaceAll('=', '');
+        eval('function '+ scriptid +'() {'+ textCode + '};');
+        // eval('let '+ scriptid +' = new function (){this.main=(()=>{'+ textCode + '});};');
+        eval(scriptid + '();');
+    }
+
+    function ImportJsScrypt(srcText, nameId="gostSRC", docLocate=document.getElementsByTagName('head')[0]){
+        let script = document.createElement('script', sedondID=null);
+        script.id = nameId + "_" + Math.floor(Math.random() * 2080);
+        script.src = srcText;
+        docLocate.appendChild(script);
+        delScript(script.id);
+        if (sedondID!=null) {delScript(sedondID);}
+    };
+
+    function ImportCssScrypt(linktext, nameId="TempStyle_", docLocate=document.getElementsByTagName('body')[0]){
+        var link = document.createElement('link');
+        link.id = nameId + '_' + Math.floor(Math.random() * 2080)
+        link.rel = 'stylesheet'; 
+        link.type = 'text/css';
+        link.href = linktext;
+        docLocate.appendChild(link);
+    };
+    
+    
+    this.rnmLoad = function(rnm){if (security0 ==true){security0=false;numerCC=rnm;}}
+    this.tempPage = function () {return SelectPage;};
+    this.load =async function (page, Documento = document.getElementsByTagName("body")[0]){
+        SelectPage = page;
+        let selectFile;
+        let ejecuteScripts;
+        let existPage=existList(cachePage[0],page);
+        if(existPage){
+            selectFile=cachePage[1][existPage-1];
+            ejecuteScripts=cachePage[2][existPage-1];
+        }else{
+            let namePage = await decode(page);
+            let litNPage = namePage.replaceAll(".html", "");
+            selectFile=await encodeInText(await readFilePhp(namePage))
+
+            
+            //selectFile=await encodeInText(await readFile(namePage))
+
+            let ifoTitle = indicators(selectFile, "<title>", "<title>".replace("t","/t"))
+            selectFile = selectFile.replaceAll(ifoTitle[0][0], "");
+            let titulo=ifoTitle.length>0?ifoTitle[1][0]:null;
+
+
+            let JS_Global_Scrypt_List=[];
+            let scriptsDeaful0 = indicators(selectFile, "<script global>", "</script>")
+            for (let i = 0; i < scriptsDeaful0[0].length; i++) {
+                selectFile = selectFile.replaceAll(scriptsDeaful0[0][i], "");
+                let editedscurityPage = 'if (ModuloLoad.tempPage()=="' + page + '") { ' + scriptsDeaful0[1][i] + ' }'
+                JS_Global_Scrypt_List.push(function() {
+                    ghostScript(editedscurityPage, "JS_" + litNPage, Documento);
+                })
+            }
+            
+            let JS_Scrypt_List=[];
+            let scriptsDeaful = indicators(selectFile, "<script>", "<script>".replace("s","/s"))
+            for (let i = 0; i < scriptsDeaful[0].length; i++) {
+                selectFile = selectFile.replaceAll(scriptsDeaful[0][i], "");
+                let editedscurityPage = 'if (ModuloLoad.tempPage()=="' + page + '") { ' + scriptsDeaful[1][i] + ' }'
+                JS_Scrypt_List.push(function() {
+                    ghostScriptEval(editedscurityPage, "JS_" + litNPage);
+                })
+            }
+            
+            let JS_SRC_Scrypt_List=[];
+            let importacionSrcScrypt = indicators(selectFile, '<script src="', '"></>')
+            for (let i = 0; i < importacionSrcScrypt[0].length; i++) {
+                selectFile = selectFile.replaceAll(importacionSrcScrypt[0][i],"");
+                JS_SRC_Scrypt_List.push(function () {
+                    ImportJsScrypt(importacionSrcScrypt[1][i], "JS_SRC_" + litNPage)
+                })
+            }
+
+            let CSS_Scrypt_List=[];
+            let importacionCss = indicators(selectFile, '<link rel="stylesheet" href="', '">')
+            for (let i = 0; i < importacionCss[0].length; i++) {
+                selectFile = selectFile.replaceAll(importacionCss[0][i],"");
+                CSS_Scrypt_List.push(function () {
+                    ImportCssScrypt(importacionCss[1][i], "CSS_", Documento);
+                })
+            }
+
+            let SpecialParrafos=indicators(selectFile, '<text>','</text>');
+            for (let i = 0; i < SpecialParrafos.length; i++) {
+                if (!SpecialParrafos[1][i]=='') {
+                    let nameTemp = 'Temp_Text' + Math.floor(Math.random() * 2080)
+                    selectFile = selectFile.replaceAll(SpecialParrafos[0][i],nameTemp);
+                    let poseced=SpecialParrafos[1][i].replaceAll('\r','\r<br>');
+                    poseced=SpecialParrafos[1][i].replaceAll('\n','\n<br>');
+                    selectFile = selectFile.replaceAll(nameTemp,poseced);
+                }
+                
+            }
+
+            ejecuteScripts = ()=>{
+                if (titulo!=null) {document.title = titulo;}
+                JS_Global_Scrypt_List.forEach(js_scrypt => {js_scrypt();});
+                CSS_Scrypt_List.forEach(Csscrypt => {Csscrypt();});
+                JS_SRC_Scrypt_List.forEach(js_src_scrypt => {js_src_scrypt();});
+                JS_Scrypt_List.forEach(js_scrypt => {js_scrypt();});
+            }
+            cachePage[0].push(page);
+            cachePage[1].push(selectFile);
+            cachePage[2].push(function() {ejecuteScripts();});
+        }
+        Documento.innerHTML=selectFile
+        ejecuteScripts();
+    }
+}
+getTagName = (name, num=0)=>{return document.getElementsByTagName(name)[num];}
